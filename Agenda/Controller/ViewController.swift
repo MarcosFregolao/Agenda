c146e0f72c0a4fb1e56eadf359c9740f7947d716
PUCPR <pucpr@ladmac0123.local>
2019-03-20 20:57:52 -0300

    Criação das telas iniciais e lista de Contatos.

---
+++ Agenda/Controller/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Agenda
+//
+//  Created by PUCPR on 18/03/19.
+//  Copyright © 2019 PUCPR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    /*
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "quiz" {
+            let next = segue.destination as! QuizViewController
+            next.quiz = quiz
+        }
+        else if segue.identifier == "editar" {
+            let next = segue.destination as! QuestionTableViewController
+            next.quiz = quiz
+        }
+    }
+    */
+    
+}
+
